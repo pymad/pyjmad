ef363af451f9534e8e30bf1f95d3552bd0335a3f
Michi <michi.hostettler@cern.ch>
2017-05-18 15:06:50 +0200

    fix setup on virtualenv

--- setup.py
+++ setup.py
@@ -5,10 +5,16 @@ import setuptools
 from setuptools.command.install import install as _install
 
 import pyjmad
+import os, site
 
 class install(_install):
     def run(self):
         print('registering cmmnbuild_dep_manager')
+        if not hasattr(site, 'getusersitepackages'):
+            print('running in VirtualEnv, monkey-patching site module')
+            site.getusersitepackages = lamda: '/home/travis/virtualenv/python3.5.2/lib/python3.5/site-packages/'
+            print(os.environ['VIRTUAL_ENV'])
+            print(os.environ['PYTHONPATH'])
         import cmmnbuild_dep_manager
         mgr = cmmnbuild_dep_manager.Manager()
         mgr.install('pyjmad')
