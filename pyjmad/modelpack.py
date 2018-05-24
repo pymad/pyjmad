19fe488682941a842aa006fa431cb3c7f0ef2a4f
mihostet <michi.hostettler@cern.ch>
2018-05-24 15:17:41 +0200

    enable and disable of repos

--- pyjmad/modelpack.py
+++ pyjmad/modelpack.py
@@ -1,8 +1,42 @@
 import itertools
 import logging
 
-from .pyjmad import org
+from .pyjmad import org, java
 from .util import *
+
+
+def _repo_to_uri(javaRepo):
+    GitlabModelPackageRepository = org.jmad.modelpack.service.gitlab.GitlabModelPackageRepository
+    InternalRepository = org.jmad.modelpack.service.internal.domain.InternalRepository
+    if type(javaRepo) is InternalRepository:
+        return 'jmad:internal'
+    elif type(javaRepo) is GitlabModelPackageRepository:
+        return javaRepo.baseUrl() + '/' + javaRepo.groupName()
+    else:
+        raise ValueError('unknown repository type: ' + str(javaRepo))
+
+
+def _repo_from_uri(uri):
+    GitlabModelPackageRepository = org.jmad.modelpack.service.gitlab.GitlabModelPackageRepository
+    InternalRepository = org.jmad.modelpack.service.internal.domain.InternalRepository
+    schema = uri.split(':')[0]
+    if schema == 'jmad':
+        if uri == 'jmad:internal':
+            return InternalRepository.INTERNAL
+        else:
+            raise ValueError('invalid "jmad:" uri "{0}" - expected = jmad:internal'.format(uri))
+    elif schema == 'http' or schema == 'https':
+        base_url, group_name = uri.rsplit('/', 1)
+        return GitlabModelPackageRepository(base_url, group_name)
+    else:
+        raise ValueError('invalid repository URI: ' + uri)
+
+
+jmad_default_repositories = {}
+for method in org.jmad.modelpack.JMadModelRepositories.__javaclass__.getDeclaredMethods():
+    if method.getModifiers() & java.lang.reflect.Modifier.PUBLIC:
+        repo = method.getName()
+        jmad_default_repositories[repo] = _repo_to_uri(getattr(org.jmad.modelpack.JMadModelRepositories, repo)())
 
 
 class JMadModelPackService(object):
@@ -13,15 +47,27 @@ class JMadModelPackService(object):
         self._reload()
 
     def _reload(self):
+        def modelpack_name(pkg):
+            return str(pkg.modelPackage().name())
+
         logging.info('Fetching available ModelPacks from ' + str(self.repositories))
         self._modelpacks = {}
-        modelpacks = self._javaService.availablePackages().collectList().block()
-        for name, variants in itertools.groupby(modelpacks, lambda m: m.modelPackage().name()):
+        modelpacks = list(self._javaService.availablePackages().collectList().block())
+        modelpacks.sort(key=modelpack_name)
+        for name, variants in itertools.groupby(modelpacks, modelpack_name):
             self._modelpacks[name] = ModelPackType(self, name, variants)
 
     @property
     def repositories(self):
-        return self._javaRepositoryManager.enabledRepositories().collectList().block()
+        return [_repo_to_uri(r) for r in self._javaRepositoryManager.enabledRepositories().collectList().block()]
+
+    def add_repository(self, repo_uri):
+        self._javaRepositoryManager.enable(_repo_from_uri(repo_uri))
+        self._reload()
+
+    def remove_repository(self, repo_uri):
+        self._javaRepositoryManager.disable(_repo_from_uri(repo_uri))
+        self._reload()
 
     def __getitem__(self, modelpack):
         return self._modelpacks[modelpack]
@@ -40,8 +86,8 @@ class JMadModelPackService(object):
 
     def __repr__(self):
         return 'ModelPackService [ \n' \
-               ' - Enabled Repos: ' + str(self.repositories) + '\n' + \
-               ' - Available ModelPacks: ' + str(self._modelpacks) + '\n' + \
+               ' -> Enabled Repos: \n    - ' + '\n    - '.join(self.repositories) + '\n' + \
+               ' -> Available ModelPacks: \n    - ' + '\n    - '.join(self._modelpacks.keys()) + '\n' + \
                ']'
 
     def __str__(self):
